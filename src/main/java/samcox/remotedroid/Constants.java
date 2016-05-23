8483a12622cd13cbbb014de08f9361bc810f7552
Sam Cox <samcox@Sams-MacBook-Pro.local>
2016-05-23 23:28:55 +0100

    Initial commit

---
+++ src/main/java/samcox/remotedroid/Constants.java
@@ -0,0 +1,14 @@
+package samcox.remotedroid;
+
+/**
+ * Created by samcox on 25/02/2016.
+ */
+public class Constants {
+
+    public static String SERVER_IP = "10.8.4.0";
+    public static int SERVER_PORT = 8998;
+    public static String PLAY = "play";
+    public static String NEXT = "next";
+    public static String PREVIOUS = "previous";
+    public static String MOUSE_LEFT_CLICK = "left_click";
+}
